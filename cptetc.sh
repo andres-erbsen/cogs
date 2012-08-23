064ad0f1b02b784a01d2c1e9df975c311379aac8
Andres Erbsen <andres.erbsen@gmail.com>
2012-08-23 15:25:14 +0300

    cpetch remove pmutils

--- cptetc.sh
+++ cptetc.sh
@@ -4,7 +4,6 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 rm -rf ${DIR}/etc/*
 cp -art "${DIR}/etc"          \
-  /etc/pm                     \
   /etc/rc.conf                \
   /etc/cdmrc                  \
   /etc/acpi/handler.sh
