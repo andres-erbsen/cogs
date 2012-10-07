a63a9ac88dc60ac7b686f97f2a62b26729b2a36c
Andres Erbsen <andres.erbsen@gmail.com>
2012-10-07 18:40:53 +0300

    /etc stuff

--- cptetc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-
-rm -rf ${DIR}/etc/*
-cp -art "${DIR}/etc"          \
-  /etc/rc.conf                \
-  /etc/cdmrc                  \
-  /etc/acpi/handler.sh
