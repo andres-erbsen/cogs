d38f1b6ec0f48b60ba2792e77ac297bb0355d71c
Andres Erbsen <andres@krutt.org>
2013-11-17 04:24:00 +0200

    mutt-gpg

---
+++ cogs.sh
@@ -0,0 +1,15 @@
+cp ~/.mpoprc ~/cogs/mpoprc
+cp ~/.msmtprc ~/cogs/msmtprc
+cp ~/.muttrc ~/cogs/muttrc
+cp ~/.gitconfig ~/cogs/gitconfig
+cp ~/.notmuch-config ~/cogs/notmuch-config
+cp ~/.shellrc ~/cogs/shellrc
+cp ~/.ssh/config ~/cogs/ssh_config
+cp ~/.xmonad/xmonad.hs ~/cogs/xmonad.hs
+cp ~/.xprofile ~/cogs/xprofile
+cp ~/.xinitrc ~/cogs/xinitrc
+cp ~/.env ~/cogs/env
+cp ~/.zshrc ~/cogs/zshrc
+cp ~/.Xresources ~/cogs/Xresources
+cp ~/.gnupg/gpg.conf ~/cogs/gpg.conf; # MKCOGS
+cp /etc/Muttrc.gpg.dist ~/cogs/etc/Muttrc.gpg.dist
