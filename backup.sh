0cd4eff7f0ba86543562e2ef5bc47efeaf723e84
Andres Erbsen <andres@krutt.org>
2013-11-17 08:49:25 +0200

    backup

---
+++ backup.sh
@@ -0,0 +1,3 @@
+cd
+rsync -ruv --inplace --size-only --partial movies music pictures /mnt/pocke/
+rsync -ruv --inplace --size-only --partial archive cogs .config Dropbox .electrum .gitconfig .gnupg .irssi .jitsi .keyring mail .mpoprc .msmtprc .muttrc .notmuch-config .purple .shellrc .ssh .xmonad .xprofile .zprofile .zshrc /mnt/pocke/andres/
