132e46c3ee94556517505c0c00701814417603af
Andres Erbsen <andres@krutt.org>
2014-01-02 22:19:10 +0200

    gpg-agent in fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,10 @@
 if status --is-login
-        set PATH $PATH /usr/bin /sbin /usr/local/bin /home/andres/Dropbox/t/programming/go/bin
+	set PATH $PATH /usr/bin /sbin /usr/local/bin /home/andres/Dropbox/t/programming/go/bin
 end
 set TORMIT 18.187.1.68
 set TORMIT2 18.238.1.85
+
+cat ~/.gpg-agent-info | sed 's/=/ /' | while read key value
+	set -e $key
+	set -U -x $key "$value"
+end
