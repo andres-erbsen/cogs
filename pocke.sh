280c72a9c3e7d3eb9071ba57073fa42cd33270f7
Andres Erbsen <andres@krutt.org>
2014-05-02 15:34:35 -0400

    rename backup -> pocke

--- pocke.sh
+++ pocke.sh
@@ -1,4 +1,5 @@
-rdiff-backup -v5 --include-globbing-filelist ~/cogs/backup-filelist.txt ~/ /mnt/pocke/andres/
+fusermount -u /home/andres/temp/0
+rdiff-backup -v5 --include-globbing-filelist ~/cogs/pocke-filelist.txt ~/ /mnt/pocke/andres/
 for annex in ~/archive ~/movies ~/music ~/pictures; do
 	pushd "$annex"
 	git annex sync pocke
