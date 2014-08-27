20c476cf33b1e83603873a676640d01437399cbe
Andres Erbsen <andres@krutt.org>
2014-08-27 06:08:47 -0700

    chromium; notmuch-notify

---
+++ notmuch-notify.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# INSTALL:
+# mkdir ~/mail/.notmuch/hooks
+# cp notmuch-notify.sh ~/mail/.notmuch/hooks/post-new
+# chmod +x ~/mail/.notmuch/hooks/post-new
+
+notmuch tag -unread folder:sent and tag:unread
+
+for msgid in $(notmuch search --output=messages tag:unnotified); do
+	msgjson=$(notmuch show --format=json --body=false --entire-thread=false "$msgid")
+	subject=$(echo "$msgjson" | jq 'recurse(.[]?) | .Subject? | select(length > 0)' -r)
+	sender=$(echo "$msgjson" | jq 'recurse(.[]?) | .From? | select(length > 0)' -r)
+	if echo "$sender" | grep -q '<'; then
+		sender=$(echo "$sender" | cut -d '<' -f2 | cut -d '>' -f1)
+	fi
+	env -i $(cat /proc/$(pgrep -u $USER xmonad)/environ | tr '\0' ' ') notify-send "$sender" "$subject"
+	echo env -i $(cat /proc/$(pgrep -u $USER xmonad)/environ | tr '\0' ' ') notify-send "$sender" "$subject"
+done
+notmuch tag -unnotified tag:unnotified
--- zshrc
+++ zshrc
@@ -1,2 +1,5 @@
-. ~/.env
-. ~/.shellrc
+export BATTERY=1
+
+for file in /etc/zsh/zshrc /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ~/.env; do
+	[ -f "$file" ] && source "$file"
+done
