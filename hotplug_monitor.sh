62d92fc47d836171830bc623bb9ed9b92a4298bd
Andres Erbsen <andres@krutt.org>
2015-03-14 17:09:50 -0400

    hotplug_monitor.sh

--- hotplug_monitor.sh
+++ hotplug_monitor.sh
@@ -6,7 +6,8 @@ export XAUTHORITY=/home/andres/.Xauthority
 function connect(){
 	read VGA_WIDTH  VGA_HEIGHT  < <( xrandr -q | awk -F'[ \t]+|x' 'found==1 && /.*\+.*/ {print($2, $3); exit}; /VGA1/ { found=1 }' )
 	read LVDS_WIDTH LVDS_HEIGHT < <( xrandr -q | awk -F'[ \t]+|x' 'found==1 && /.*\+.*/ {print($2, $3); exit}; /LVDS1/ { found=1 }' )
-	xrandr --output VGA1 --auto --pos 0x0 --output LVDS1 --primary --pos "${VGA_WIDTH}x$((${VGA_HEIGHT}-${LVDS_HEIGHT}))"
+	xrandr --output VGA1 --primary --auto --pos 0x0 --output LVDS1 --pos "${VGA_WIDTH}x$((${VGA_HEIGHT}-${LVDS_HEIGHT}))"
+	setxkbmap ee -variant us -option caps:escape
 }
  
 function disconnect(){
